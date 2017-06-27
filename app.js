c0f9f590c2b80654b38fed88b94878fb5b57fd7f
Pogrebnyak Alex <pogrebnyakalexx@gmail.com>
2017-06-28 01:59:58 +0300

    Adding Forecast functional

--- app.js
+++ app.js
@@ -14,6 +14,10 @@ weatherApp.config(function($routeProvider){
         templateUrl: 'pages/forecast.htm',
         controller: 'forecastController'
     })
+    .when('/forecast/:days',{
+        templateUrl: 'pages/forecast.htm',
+        controller: 'forecastController'
+    });
  
 });
 
@@ -35,6 +39,29 @@ weatherApp.controller('homeController', ['$scope', 'cityService',function($scope
     
 }]); 
 
-weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope,cityService){
+weatherApp.controller('forecastController', ['$scope', '$resource', '$routeParams', 'cityService', function($scope,$resource,$routeParams,cityService){
+    
     $scope.city = cityService.city;
+    $scope.days = $routeParams.days || 2;
+    
+    $scope.weatherAPI = $resource(
+    'http://api.openweathermap.org/data/2.5/forecast',
+    {callback: 'JSON_CALLBACK'}, 
+    {get: {method: 'JSONP'}});
+    
+    $scope.weatherResult = $scope.weatherAPI.get({
+        q: $scope.city,
+        APPID: '90de47701517213da4a09993d9d3e008',
+        cnt: $scope.days 
+    });
+    
+    $scope.convertToCelsius = function(degK){
+        return Math.round(degK-273.15);
+    }
+    
+    $scope.convertToDate = function(date){
+        return new Date(date*1000);
+    }
+    
+    console.log($scope.weatherResult);
 }]); 
