abc75e6fc0b15123e58864a2210b987410a51998
Pogrebnyak Alex <pogrebnyakalexx@gmail.com>
2017-06-27 14:29:58 +0300

    Adding custom service

--- app.js
+++ app.js
@@ -17,13 +17,24 @@ weatherApp.config(function($routeProvider){
  
 });
 
+//Adding custom service
+
+weatherApp.service('cityService',function(){
+    this.city = "New York, NY"
+});
+
 
 //Adding controllers
 
-weatherApp.controller('homeController', ['$scope', function($scope){
+weatherApp.controller('homeController', ['$scope', 'cityService',function($scope,cityService){
+    $scope.city = cityService.city;
+    
+    $scope.$watch('city', function(){
+        cityService.city = $scope.city;
+    });
     
 }]); 
 
-weatherApp.controller('forecastController', ['$scope', function($scope){
-    
+weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope,cityService){
+    $scope.city = cityService.city;
 }]); 
